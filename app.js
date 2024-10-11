c00db3811d8c6015b1b6acc6bd56a707717da580
Ahnaf <mohammadahnaf123@gmail.com>
2024-10-11 17:57:28 +0400

    Complete model in prisma
    Define some routes for users

--- app.js
+++ app.js
@@ -4,6 +4,22 @@ import jwt from "jsonwebtoken";
 
 const app = express();
 
+app.get("/users", (req, res) => {
+  // get all users
+})
+
+app.get("/users/:id", (req, res) => {
+  // get a single user
+})
+
+app.post("users/register", (req, res) => {
+  // register a new user
+})
+
+app.post("/users/login", (req, res) => {
+  // authenticate user JWT
+})
+
 app.listen(process.env.PORT, () =>
   console.log(`listening on port ${process.env.PORT}`)
 );
---
+++ prisma/migrations/20241011133604_init/migration.sql
@@ -0,0 +1,47 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('USER', 'AUTHOR');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "firstName" VARCHAR(255) NOT NULL,
+    "lastName" VARCHAR(255) NOT NULL,
+    "password" VARCHAR(255) NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'USER',
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "posts" (
+    "id" TEXT NOT NULL,
+    "title" VARCHAR(255) NOT NULL,
+    "content" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "published" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "posts_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "comments" (
+    "id" TEXT NOT NULL,
+    "text" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "postId" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "comments_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "posts" ADD CONSTRAINT "posts_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "comments" ADD CONSTRAINT "comments_postId_fkey" FOREIGN KEY ("postId") REFERENCES "posts"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "comments" ADD CONSTRAINT "comments_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
