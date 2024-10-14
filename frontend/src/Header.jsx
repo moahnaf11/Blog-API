60576df4abc4bab5e9054d9b6171721a526e32b3
Ahnaf <mohammadahnaf123@gmail.com>
2024-10-14 15:31:53 +0400

    Create a React App for the front end
    Created react app
    Created a router using react router
    Created a reusable Header component for the homepage

---
+++ frontend/src/Header.jsx
@@ -0,0 +1,26 @@
+import { NavLink } from "react-router-dom";
+
+function Header() {
+  return (
+    <>
+      <header className="p-3 flex justify-between items-center gap-5 bg-yellow-300 flex-col sm:flex-row">
+        <h1 className="font-custom font-bold text-2xl">Blog Haven</h1>
+        <nav>
+          <ul className="flex items-center gap-3">
+            <li>
+              <NavLink className="font-custom inline-block font-bold border-2 border-black p-2" to="/login">Login</NavLink>
+            </li>
+            <li>
+              <NavLink className="font-custom inline-block font-bold border-2 border-black p-2" to="/register">Register</NavLink>
+            </li>
+            <li>
+              <NavLink className="font-custom inline-block font-bold border-2 border-black p-2"  to="/logout">Log out</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
+
+export { Header };
---
+++ frontend/src/main.jsx
@@ -0,0 +1,28 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      // {
+      //   path: "login",
+      //   element: <Login />,
+      // },
+      // {
+      //   path: "register",
+      //   element: <Register />,
+      // },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
